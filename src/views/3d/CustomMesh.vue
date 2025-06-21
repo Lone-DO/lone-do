031a3ca29298d61e541874fb5188bed89ca3e46f
lone-DO <citizin@outlook.com>
2025-06-21 14:56:12 +0200

    Linting Configs

--- src/views/3d/CustomMesh.vue
+++ src/views/3d/CustomMesh.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
-import { extend, useTresContext } from '@tresjs/core';
-import { FlakesTexture } from 'three/examples/jsm/Addons.js';
+import { extend } from '@tresjs/core'
+import { FlakesTexture } from 'three/examples/jsm/Addons.js'
 extend({ FlakesTexture })
 </script>
 
 <template>
   <TresMesh>
-    <TresTorusGeometry :args='[1, .5, 16, 32]' />
-    <TresMeshLambertMaterial color='#468585' emissive='#460505' />
+    <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+    <TresMeshLambertMaterial color="#468585" emissive="#460505" />
     <FlakesTexture />
   </TresMesh>
 </template>
